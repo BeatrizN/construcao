c8674531231b28258c7f7a9ae547e606334b36c2
alunoinf <alunoinf@pc-649301.(none)>
2015-09-24 19:28:41 -0300

    Correcao

--- polsimples/src/main/java/home/alunoinf/construcao/polsimples/Polsimples.java
+++ polsimples/src/main/java/home/alunoinf/construcao/polsimples/Polsimples.java
@@ -4,24 +4,22 @@ package home.alunoinf.construcao.polsimples;
  * Classe para calcular polinomios
  */
 
-    import java.lang.Object;
-    import java.lang.Math;
+import java.lang.Math;
 
-        public class Polsimples {
-        public static int polsimples(int x, int g, int[]  a) {
-            if ((g < 1) || (a [g] == 0)) {
-                throw new IllegalArgumentException("g menor que zero ou a igual a zero");
-            }
-            double p = a [0];
-            int i = 1;
-            double t =  Math.pow(x, g);
-
-            while(i <= g) {
-                t =  Math.pow(x, i);
-                p =  (p + a[i] * t);
-                i += 1;
-            }
-            return (int)p;
+public class Polsimples {
+    public static int polsimples(int x, int g, int[] a) {
+        if ((g < 1) || (a[g] == 0)) {
+            throw new IllegalArgumentException("g menor que zero ou a igual a zero");
         }
+        double p = a[0];
+        int i = 1;
+        double t = Math.pow(x, g);
+
+        while (i <= g) {
+            t = Math.pow(x, i);
+            p = (p + a[i] * t);
+            i += 1;
+        }
+        return (int) p;
     }
 }
--- polsimples/src/test/java/home/alunoinf/construcao/polsimples/PolsimplesTest.java
+++ polsimples/src/test/java/home/alunoinf/construcao/polsimples/PolsimplesTest.java
@@ -7,23 +7,27 @@ public class PolsimplesTest {
 
 
     @Test(expected = IllegalArgumentException.class)
-    public void testPrimeiroParametroInvalido() throws Exception {
-        Polsimples.polsimples(2, 3, int[] a = {8 ,2 ,9, 5} );
+    public void testSegundoParametroInvalido() throws Exception {
+        int a[] = {1, 0 ,9, 5};
+        Polsimples.polsimples(2, 0, a);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testsegundoParametroInvalido() throws Exception {
-        Polsimples.polsimples(3, 0, {2, 7, 0});
+    public void testUltimoParametroInvalido() throws Exception {
+        int [] a = {2, 7, 0};
+        Polsimples.polsimples(3, 0, a);
     }
 
     @Test
     public void testCasosDeFronteira() {
-        Assert.assertEquals(99, Polsimples.polsimples(2, 4, {3, 2, 5, 1, 4}));
+        int [] a = {3, 2, 5, 1, 4};
+        Assert.assertEquals(99, Polsimples.polsimples(2, 4, a));
     }
 
     @Test
     public void testCasoTrivial() {
-        Assert.assertEquals(-11, Polsimples.polsimples( 1, 3, {3, -2, -4, -8}));
+        int [] a = {3, -2, -4, -8};
+        Assert.assertEquals(-11, Polsimples.polsimples( 1, 3, a));
 
     }
 }
